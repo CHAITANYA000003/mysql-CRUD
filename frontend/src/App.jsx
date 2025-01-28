926bd19d9ef58ca50bd847ae57af2294b1e9c14b
CHAITANYA000003 <chaitanya2003217@gmail.com>
2025-01-28 17:30:26 +0530

    Add styles to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,20 @@ import Add from "./pages/Add";
 import Books from "./pages/Books";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Update from "./pages/Update";
+import "./style.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/update" element={<Update />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/update" element={<Update />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
