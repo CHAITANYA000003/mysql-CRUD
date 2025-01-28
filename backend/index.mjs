bf7918486ab04deb24d538916c26bdb85b4cf304
CHAITANYA000003 <chaitanya2003217@gmail.com>
2025-01-28 11:43:10 +0530

    Establish connection to database

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,7 +1,29 @@
 import express from "express";
+import mysql from "mysql2";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+const TABLE = process.env.MYSQL_TABLE_NAME;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+});
+
+app.get("/books", (req, res) => {
+  const q = `SELECT * FROM ${TABLE}`;
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.json(data);
+    }
+  });
+});
 
 app.listen(8080, () => {
-  console.log("Server listening on port 8080");
+  console.log(`Server listening on port ${PORT}`);
 });
