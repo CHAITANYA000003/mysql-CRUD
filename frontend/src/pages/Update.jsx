2ede19062b050a52d69991263db04d9706a6423b
CHAITANYA000003 <chaitanya2003217@gmail.com>
2025-01-28 17:48:30 +0530

    Add update page, and style add and update pages

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const URL = "http://localhost:8080";
 
 const Update = () => {
-  return <div>Update</div>;
+  const [book, setBook] = useState({
+    title: "",
+    about: "",
+    price: null,
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${URL}/books`, book);
+      //   we want to navigate back to the home page once the button is clicked, we do this using the useNavigate of react-router-dom
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Update the Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="about"
+        onChange={handleChange}
+        name="about"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button className="formButton" onClick={handleClick}>
+        Update
+      </button>
+    </div>
+  );
 };
 
 export default Update;
