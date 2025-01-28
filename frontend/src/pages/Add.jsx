64db281f233fc2859fa6db544e8817f747afc9e4
CHAITANYA000003 <chaitanya2003217@gmail.com>
2025-01-28 17:18:52 +0530

    Implement Add page in frontend

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const URL = "http://localhost:8080";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const [book, setBook] = useState({
+    title: "",
+    about: "",
+    price: null,
+    cover: "",
+  });
 
-export default Add
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${URL}/books`, book);
+      //   we want to navigate back to the home page once the button is clicked, we do this using the useNavigate of react-router-dom
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="about"
+        onChange={handleChange}
+        name="about"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button onClick={handleClick}>Add</button>
+    </div>
+  );
+};
+
+export default Add;
